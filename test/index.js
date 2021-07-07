49662db99686f55695a462bf95ba076ec8a0dc70
谢维林 <xieweilin@ele-cloud.com>
2021-07-07 18:08:08 +0800

    asd

---
+++ test/index.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: 谢维林
+ * @Date: 2021-07-07 10:48:46
+ * @LastEditTime: 2021-07-07 11:08:02
+ * @LastEditors: 谢维林
+ * @Description: 
+ * @FilePath: /homeWork/test/index.js
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
+
+export default class BindEvent extends ReadableStreamDefaultController.Component {
+
+    constructor(){
+
+    }
+}
+
+
+//#region 
+asdada
+//#endregion
